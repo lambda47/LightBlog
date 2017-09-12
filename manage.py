163aaf65cd19990d9623908236bcb12841f34f1a
Lambda47 <geek@life-and.me>
2017-09-13 00:24:27 +0800

    替换jinja默认变量分隔符

--- manage.py
+++ manage.py
@@ -3,6 +3,9 @@ from app.admin.admin import admin as admin_blueprint
 
 app = Flask(__name__, static_folder='app/static', template_folder='app/templates')
 
+app.jinja_env.variable_start_string = '${'
+app.jinja_env.variable_end_string = '}'
+
 app.register_blueprint(admin_blueprint, url_prefix = '/admin/user')
 
 if __name__ == '__main__':
