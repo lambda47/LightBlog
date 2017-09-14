723ad2565834b2518994fa5f18dbdec413b6a9d3
Lambda47 <geek@life-and.me>
2017-09-15 02:01:36 +0800

    引入file session

--- manage.py
+++ manage.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from app.admin.admin import admin as admin_blueprint
+from flask_session import Session
 
 app = Flask(__name__, static_folder='app/static', template_folder='app/templates')
 
 app.jinja_env.variable_start_string = '${'
 app.jinja_env.variable_end_string = '}'
+
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 
 app.register_blueprint(admin_blueprint, url_prefix = '/admin/user')
 
