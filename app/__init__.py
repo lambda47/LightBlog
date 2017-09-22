79bf4b13443e8e0be05fc2518d72ab791849dc3a
Lambda47 <geek@life-and.me>
2017-09-22 08:13:00 +0800

    修改代码结构

---
+++ app/__init__.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_session import Session
+from flask_login import LoginManager
+from flask_pymongo import PyMongo
+
+from app.admin.user import user as view_admin_user
+from app.api.user import user as api_user
+from app.model.user import User
+
+app = Flask(__name__, static_folder='static', template_folder='templates')
+app.app_context().push()
+
+app.jinja_env.variable_start_string = '${'
+app.jinja_env.variable_end_string = '}'
+
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
+app.config['SESSION_PROTECTION'] = 'strong'
+login_manager = LoginManager()
+@login_manager.user_loader
+def load_user(userid):
+    return User.find(userid)
+login_manager.init_app(app)
+
+app.config['MONGO_HOST'] = 'localhost'
+app.config['MONGO_PORT'] = 27017
+app.config['MONGO_DBNAME'] = 'lightblog'
+mongo = PyMongo(app)
+
+app.register_blueprint(view_admin_user, url_prefix = '/admin/user')
+app.register_blueprint(api_user, url_prefix = '/user')
+app.config['SECRET_KEY'] = 'LIGHTBLOG'
