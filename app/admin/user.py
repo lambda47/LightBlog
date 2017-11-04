42a72bb87dc12837e9d3c54292f6b18477935cfd
Lambda47 <geek@life-and.me>
2017-11-04 12:23:52 +0800

    管理后台退出清空session中的user信息

--- app/admin/user.py
+++ app/admin/user.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask import redirect
 from flask import url_for
 from flask import render_template
+from flask import session
 from flask_login import logout_user
 from ..lib.auth import admin_loggedin
 from ..lib.auth import admin_login_require
@@ -18,5 +19,7 @@ def login():
 @user.route('/logout', methods=['GET'])
 @admin_login_require()
 def logout():
+    if 'user' in session:
+        del session['user']
     logout_user()
     return redirect(url_for('view_admin_user.login'))
