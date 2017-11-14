e15c93254f9b14a28126e3e6163bec90f320bfc2
Lambda47 <geek@life-and.me>
2017-11-15 01:44:35 +0800

    添加图片上传组件

---
+++ source/component/imageUpload.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="tag-box-icon-wrapper" @mouseenter="enterImg()" @mouseleave="leaveImg" @dragenter="enterImg()">
+        <img class="image" :src="imgUrl"/>
+        <input type="file" ref="file" :name="name" class="file" @change="upload($event)" />
+        <div class="mask" v-show="showUpload()"  @dragleave="leaveImg()" @click="choose" @drop="upload($event)"><i class="iconfont icon-add"></i></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        img: {type: String, default: ''},
+        action: {type: String, default: ''},
+        name: {type: String, default: 'file'},
+        params: {type: Object, default: {}}
+    },
+
+    data: function() {
+        return {
+            isHover: false,
+            imgUrl: this.img
+        };
+    },
+
+    methods: {
+        showUpload: function() {
+            return this.imgUrl == '' || this.isHover;
+        },
+        enterImg: function() {
+            this.isHover = true;
+        },
+        leaveImg: function() {
+            this.isHover = false;
+        },
+        choose: function() {
+            this.$refs.file.click();
+        },
+        upload: function(event) {
+            if (event.type == 'change') {
+                var file = this.$refs.file.files[0];
+            } else {
+                var file = event.dataTransfer.files[0];
+            }
+            var formData = new FormData();
+            formData.append(this.$refs.file.name, file);
+            var params = this.params;
+            for (var key in params) {
+                formData.append(key, params[key]);
+            }
+            var _this = this;
+            $.ajax({
+                url: this.action,
+                type: 'post',
+                data: formData,
+                cache: false,
+                processData: false,
+                contentType: false,
+            }).then(function(data) {
+                _this.$emit('result', data);
+            });
+        }
+    },
+
+    created: function() {
+        this.showUpload();
+                // 阻止浏览器默认drop事件
+        ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(function (value, index, array) {
+                document.addEventListener(value, function (event) {
+                event.preventDefault();
+            }, false);
+        })
+    }
+}
+</script>
