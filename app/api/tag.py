c8dec5ec447a5278073556be16ba4309fb295b93
Lambda47 <geek@life-and.me>
2017-11-18 01:52:15 +0800

    添加标签编辑/添加接口

--- app/api/tag.py
+++ app/api/tag.py
@@ -20,3 +20,30 @@ def all_tags():
                     'name': tag.name
                 } for tag in tags]
     }
+
+@tag.route('/add', methods=['POST'])
+@apiview
+@admin_login_require('api')
+def add_tag():
+    """添加标签"""
+    name = request.form.get('name', '')
+    logo = request.form.get('logo', '')
+    id = Tag.add(Tag(name=name, logo=logo))
+    return {id: str(id)}
+
+@tag.route('/edit', methods=['POST'])
+@apiview
+@admin_login_require('api')
+def edit_tag():
+    """编辑标签"""
+    id = request.form.get('id', '')
+    name = request.form.get('name', '')
+    logo = request.form.get('logo', '')
+
+    tag = Tag.find(id)
+    if tag is None:
+        raise Exception('TAG_NOT_EXIST')
+
+    tag.name = name
+    tag.logo = logo
+    tag.save()
--- app/config/errno.py
+++ app/config/errno.py
@@ -4,5 +4,6 @@ errno = {
     'REQUIRE_USERNAME' : {'code': 1102, 'msg': '请填写用户名'},
     'REQUIRE_PASSWORD' : {'code': 1103, 'msg': '请填写密码'},
     'USER_NOT_EXIST': {'code': 1104, 'msg': '用户不存在'},
-    'PASSWORD_ERROR': {'code': 1105, 'msg': '密码不正确'}
+    'PASSWORD_ERROR': {'code': 1105, 'msg': '密码不正确'},
+    'TAG_NOT_EXIST': {'code': 1201, 'msg': '标签不存在'}
 }
