65bc60e95b15e64ebc5363b03894e26e09f97034
Lambda47 <geek@life-and.me>
2017-11-19 00:15:15 +0800

    修改标签排序

--- app/model/tag.py
+++ app/model/tag.py
@@ -1,16 +1,17 @@
-from ..lib.result import result
 import re
+import pymongo
 
 from .model import Model
 
 class Tag(Model):
     soft_del_key = 'deleted'
+    timestamps = True
 
     @classmethod
     def find_by_name(cls, name):
         """根据名称模糊匹配标签"""
         if name:
             name_rexexp = re.compile('.*{}.*'.format(name), re.IGNORECASE)
-            return cls.find_all({'name': name_rexexp})
+            return cls.find_all({'name': name_rexexp}).sort({'created_at': -1})
         else:
-            return cls.find_all()
+            return cls.find_all().sort('created_at', pymongo.DESCENDING)
