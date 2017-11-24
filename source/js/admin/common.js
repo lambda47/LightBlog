cb7b743745581ae1b751e8033336afb28d6335b2
Lambda47 <geek@life-and.me>
2017-11-24 22:05:35 +0800

    修改超链接错误

--- source/js/admin/common.js
+++ source/js/admin/common.js
@@ -1,6 +1,6 @@
 const urls = {
     view_admin_login: '/admin/user/login',
-    view_admin_index: '/admin',
+    view_admin_index: '/admin/',
     api_tags_find: '/tag/find',
     api_tags_add: '/tag/add',
     api_tags_edit: '/tag/edit',
